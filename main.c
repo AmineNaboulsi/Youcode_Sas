81e066a47ddc1d0fb885b9c3d3e30545379b3fc7
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-09-10 23:33:14 +0100

    Adding Option Avg to eatch Dep

--- main.c
+++ main.c
@@ -92,14 +92,16 @@ void Add_Student(Student List_Student[],Department List_Departement[],int len){
     {
         printf("\t %d-%s.\n",i+1,List_Departement[i].NameD);
     }
-    
     InvalidDep:
-    scanf("\t %d",&ID_Picked);
+    printf("choose : ");
+    scanf("%d",&ID_Picked);
     if(ID_Picked<=0 || len<ID_Picked)
         goto InvalidDep;
     //Affect Vale to Department struct imbriquer
+    List_Student[List_Counter].Department.IdDep = ID_Picked;
     gestDepartementById(ID_Picked ,&List_Student[List_Counter].Department.NameD,
                 List_Departement,len);
+                
 
     printf("\nEnter the student generate note :");
     scanf("%f",&List_Student[List_Counter].Note);
@@ -118,11 +120,11 @@ void gestDepartementById(int Id , char* Dep,Department List_Departement[],int le
 }
 void DisplayStudent(Student List_Student[]){
     printf("\n----------------------------------------------------------------------------\n");
-    printf("%-2s %-15s %-20s %-20s %-15s %-5s\n","|","First Name","Last Name","Department","Note","|") ;
-    printf("-------------------------------------------------------------------\n");
+    printf("%-2s %-10s %-15s %-20s %-20s %-15s %-5s\n","|","Id","First Name","Last Name","Department","Note","|") ;
+    printf("----------------------------------------------------------------------------\n");
     for (int i = 0; i < List_Counter; i++)
     {
-        printf("%-2s %-15s %-20s %-20s %-15.2f %-2s\n","|",List_Student[i].Firstame,List_Student[i].LastName,List_Student[i].Department.NameD,List_Student[i].Note,"|") ;
+        printf("%-2s %-10d %-15s %-20s %-20s %-15.2f %-2s\n","|",List_Student[i].Id,List_Student[i].Firstame,List_Student[i].LastName,List_Student[i].Department.NameD,List_Student[i].Note,"|") ;
         printf("----------------------------------------------------------------------------\n");
     }
     if(List_Counter==0)
@@ -133,37 +135,39 @@ void DisplayStudent(Student List_Student[]){
 }
 
 
-float GetDepartementAvg(Student List_Student[],char Departement[]){
+float GetDepartementAvg(Student List_Student[],char name[]){
     int Count_Student = 0;
-    int Avg = 0;
-    for (int i = 0; i < List_Counter; i++)
-    {
-        if(strcasecmp(List_Student[List_Counter].LastName ,Departement )){
-            Count_Student++;
-            Avg++;
+    float Avg = 0;
+     for (int ji = 0; ji < List_Counter; ji++)
+        {
+             if(strcasecmp(List_Student[ji].Department.NameD , name)==0){
+                  Count_Student++;
+                  Avg+=List_Student[ji].Note;
+              }
         }
-   }
    if(Count_Student == 0)
-      return -1 ;
+      return 0.0 ;
    else 
       return Avg/=Count_Student;
 }
 
 
-void DisplayDepartementAvg(Student List_Student[],Department List_Departement[]){
+void DisplayDepartementAvg(Student List_Student[],Department List_Departement[],int len){
     //Display Table Departement Avg
-    int len = sizeof(List_Departement) /sizeof(List_Departement[0]);
-    printf("len :%d\n",&len);
-    printf("\n-------------------------------\n");
+    printf("\n---------------------------------\n");
     printf("%-2s %-15s %-20s %-2s\n","|","Departement","Avg","|") ;
     printf("---------------------------------\n");  
     for (size_t i = 0; i < len; i++)
     {
+            float avg = 0;
+             
             printf("%-2s %-15s %.2f %-2s\n","|",
-                        List_Student[i].Department.NameD,
-                        GetDepartementAvg(List_Student , List_Student[i].Department.NameD)==-1?0.0:GetDepartementAvg(List_Student ,List_Student[i].Department.NameD),
+                        List_Departement[i].NameD,
+                        GetDepartementAvg(List_Student , List_Departement[i].NameD),
                         "|") ;
-            printf("-------------------------------------------------------------------\n");
+            printf("---------------------------------\n");
+            /*printf("id :%d var :%s note : %f",List_Student[i].Department.IdDep,List_Student[i].Department.NameD,List_Student[i].Note );
+            printf("---------------------------------\n");*/
     }
 }
 
@@ -179,15 +183,42 @@ void Fill_Department(Department List_Department[]) {
 }
 
 
+void filldata(Student List_Student[]){
+   List_Student[0].Id = 1;
+   strcpy(List_Student[0].Firstame , "aaa"); 
+   strcpy(List_Student[0].LastName , "bbb");  
+   List_Student[0].Note = 15.8 ;
+   List_Student[0].Department.IdDep = 0;
+   strcpy(List_Student[0].Department.NameD , "Biologie");   
+
+   List_Student[1].Id = 1;
+   strcpy(List_Student[1].Firstame , "gggg"); 
+   strcpy(List_Student[1].LastName , "yyyy");  
+   List_Student[1].Note = 11.8 ;
+   List_Student[1].Department.IdDep = 1;
+   strcpy(List_Student[1].Department.NameD , "Informatique");  
+
+   List_Student[2].Id = 2;
+   strcpy(List_Student[2].Firstame , "eeee"); 
+   strcpy(List_Student[2].LastName , "yyyy");  
+   List_Student[2].Note = 17.8 ;
+   List_Student[2].Department.IdDep = 1;
+   strcpy(List_Student[2].Department.NameD , "Informatique");  
+
+   List_Counter = 3; 
+}
 int main(){
     //Option picked
     int Picked_Option ;
     //Student List
     Student List_Student[List_Length];
     //Department List
-    Department List_Department[5];
+    Department List_Department[12];
     //Fill dataset
     Fill_Department(List_Department);
+
+    //Fill test data 
+    filldata(List_Student);
     do{
         printf("%-70s\n","_______________________________________________________________");
         printf("%-5s %-60s \n","","1. Add student");        
@@ -209,7 +240,7 @@ int main(){
             DisplayStudent(List_Student);
             break;
         case 5:
-            DisplayDepartementAvg(List_Student ,List_Department);
+            DisplayDepartementAvg(List_Student ,List_Department,sizeof(List_Department) / sizeof(List_Department[0]));
             break;
         default:
             break;
